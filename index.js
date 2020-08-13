904cbae2ef45e4289430f6c2a6bad8899a2c7331
Leo <lpolicastro@pm.me>
2020-08-13 09:01:57 -0400

    add sequelize boilerplate

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = require('./server/app'),
+  { sequelize } = require('./server/db/models'),
   port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Express server is up on port ${port}`));
+// Sync database and spin up server
+sequelize.sync().then(async () => {
+  app.listen(port, () =>
+    console.info(`Express router listening on port ${port}!`)
+  );
+});
--- server/app.js
+++ server/app.js
@@ -1,15 +1,13 @@
 const express = require('express'),
-  path = require('path');
-
-const app = express();
+  path = require('path'),
+  app = express();
 
 //Middleware
 app.use(express.json());
 
-// Serve any static files
+// Serve cra build files
 if (process.env.NODE_ENV === 'production')
   app.use(express.static(path.join(__dirname, '../client/build')));
-
 // Any authentication middleware and related routing would be here.
 
 // Handle React routing, return all requests to React app
